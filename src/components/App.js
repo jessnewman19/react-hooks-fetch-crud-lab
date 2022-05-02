997e04c7da06673b2ddfcd9106903549dbb8b1ea
Jessica <jess.newman19@gmail.com>
2022-05-02 11:25:26 -0400

    commit tree f7dcd72a162e25c744ecc4af7dbef7fdec34c636

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [triviaQuestions, setTriviaQuestions] = useState([])
+
+  useEffect(() => { 
+    fetch("http://localhost:4000/questions")
+    .then(response => response.json())
+    .then(questions => setTriviaQuestions(questions))
+  }, [])
+
+  function handleNewItem (newQuestion) { 
+    setTriviaQuestions([...triviaQuestions, newQuestion])
+  }
+
+  function handleDeleteItem (deletedQuestion) { 
+    const updatedItems = triviaQuestions.filter(question => question.id !== deletedQuestion.id)
+    setTriviaQuestions(updatedItems)
+  }
+
+  function itemChange (updatedAnswer) { 
+    const updatedQuestions = triviaQuestions.map(question => { 
+      if (question.id === updatedAnswer.id) { 
+        return updatedAnswer
+      }
+      return question
+    })
+    setTriviaQuestions(updatedQuestions)
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm onHandleNewItem={handleNewItem}/> : <QuestionList questions={triviaQuestions} onHandleDeleteItem={handleDeleteItem} onItemChange={itemChange}/>}
     </main>
   );
 }
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onHandleDeleteItem, onItemChange }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,37 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDeleteClick () { 
+    fetch(`http://localhost:4000/questions/${question.id}`, { 
+      method: "DELETE",
+    })
+    .then(response => response.json())
+    .then(()=> onHandleDeleteItem(question))
+  }
+
+  function handleItemChange (event) { 
+    fetch(`http://localhost:4000/questions/${question.id}`, { 
+      method: "PATCH",
+      headers: { 
+        "Content-Type": "application/json"
+      }, 
+      body: JSON.stringify({ 
+        correctIndex: parseInt(event.target.value),
+      }),
+    })
+    .then(response => response.json())
+    .then(updatedAnswer => onItemChange(updatedAnswer))
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={handleItemChange} defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
